1dce82796ac032efcc808366e71fd05a8c188952
李健 <2435024850@qq.com>
2023-05-03 10:45:44 +0800

    无

--- service-oa/src/main/java/com/ys/oa/Main.java
+++ service-oa/src/main/java/com/ys/oa/Main.java
@@ -2,6 +2,7 @@ package com.ys.oa;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello 1 world!");
     }
 }
+
