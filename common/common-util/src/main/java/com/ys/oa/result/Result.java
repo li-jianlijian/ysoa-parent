e4e6b7f0b128f37a6d41df92ae41ca62f560a7ca
李健 <2435024850@qq.com>
2023-05-06 13:09:00 +0800

    统一返回结果

---
+++ common/common-util/src/main/java/com/ys/oa/result/Result.java
@@ -0,0 +1,61 @@
+package com.ys.oa.result;
+
+public class Result {
+
+    private Integer code;
+    private String message;
+    private Object data;
+
+    //构造方法 全为private
+    private Result(){}
+
+    private Result(Integer code,String message,Object data){
+        this.code = code;
+        this.message = message;
+        this.data = data;
+    }
+
+    private Result(ResultCodeEnum resultCodeEnum,Object data){
+        this.code = resultCodeEnum.getCode();
+        this.message = resultCodeEnum.getMessage();
+        this.data = data;
+    }
+
+    //成功
+    public static Result ok(){
+        return new Result(ResultCodeEnum.SUCCESS,null);
+    }
+
+    public static Result ok(String message){
+        return new Result(ResultCodeEnum.SUCCESS.getCode(),message,null);
+    }
+
+    public static Result ok(Object data){
+        return new Result(ResultCodeEnum.SUCCESS,data);
+    }
+
+    public static Result ok(String message,Object data){
+        return new Result(ResultCodeEnum.SUCCESS.getCode(),message,data);
+    }
+    //失败
+    public static Result fail(){
+        return new Result(ResultCodeEnum.FAIL,null);
+    }
+
+    public static Result fail(String message){
+        return new Result(ResultCodeEnum.FAIL.getCode(),message,null);
+    }
+
+    //set方法
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+}
